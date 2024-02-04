8762a5dfe45544dff1f16e30363fb650354d9ef3
JaredWillClark <jaredwillclark@outlook.com>
2024-02-03 23:54:41 -0500

    Create components folder for login components

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Disney+ Clone!</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/">
+          <Route exact path="/" element={<Login />}></Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
